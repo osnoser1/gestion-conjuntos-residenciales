d5ca9823e7b044bd2d4819075315959f7b5be83d
Felicce <exosymmetry@hotmail.com>
2014-06-07 00:24:52 -0430

    Modificaciones pagos-pendientes

--- js/web/PagosPendientesCtrl.js
+++ js/web/PagosPendientesCtrl.js
@@ -5,18 +5,17 @@
  */
 'use strict';
 /* Controllers */
-
 var myApp = angular.module('myApp');
 
 myApp.controllerProvider.register('PagosPendientesCtrl', function($scope, $http, $q, $filter, $timeout, $rootScope) {
     console.log('PagosPendientesCtrl');
     $scope.datos = {
-        consignaciones: [
-		    {"idConsignacion": "1", "nombre": "Ricardo","apellido": "Felicce",	"monto": "870",	"nro_referencia": "123456789"	},
-		    {"idConsignacion": "2", "nombre": "Jenny", 	"apellido": "Gonzales",	"monto": "300",	"nro_referencia": "234567890"	},
-		    {"idConsignacion": "3", "nombre": "Carlos", "apellido": "Salazar",	"monto": "980",	"nro_referencia": "345678912"	},
-		    {"idConsignacion": "4", "nombre": "Andrés", "apellido": "Freites",	"monto": "540",	"nro_referencia": "456789123"	},
-		    {"idConsignacion": "5", "nombre": "Luna", 	"apellido": "Lopez",	"monto": "230",	"nro_referencia": "567891234"	}
+        pagos: [
+		    {"id": "1", "nombre": "Ricardo","apellido": "Felicce",	"monto": "870",	"nro_referencia": "123456789", "tipo": "Depósito", "banco": "Banesco", "fecha": "08/06/2014", "estado": "Pendiente"},
+		    {"id": "2", "nombre": "Jenny", 	"apellido": "Gonzales",	"monto": "300",	"nro_referencia": "234567890", "tipo": "Depósito", "banco": "Banesco", "fecha": "08/06/2014", "estado": "Pendiente"},
+		    {"id": "3", "nombre": "Carlos", "apellido": "Salazar",	"monto": "980",	"nro_referencia": "345678912", "tipo": "Depósito", "banco": "Banesco", "fecha": "08/06/2014", "estado": "Pendiente"},
+		    {"id": "4", "nombre": "Andrés", "apellido": "Freites",	"monto": "540",	"nro_referencia": "456789123", "tipo": "Depósito", "banco": "Banesco", "fecha": "08/06/2014", "estado": "Pendiente"},
+		    {"id": "5", "nombre": "Luna", 	"apellido": "Lopez",	"monto": "230",	"nro_referencia": "567891234", "tipo": "Depósito", "banco": "Banesco", "fecha": "08/06/2014", "estado": "Pendiente"}
 		],
     };
 
@@ -31,12 +30,44 @@ myApp.controllerProvider.register('PagosPendientesCtrl', function($scope, $http,
         $scope.showConfirmDialog({title: "Aviso", message: "¿Seguro que desea rechazar esta consignación?"}, $scope.rechazarPago);
     };
 
+    $scope.pagosPendientesVerDetalles = function(element){
+    	var message = '<div class="container"><form class="form-horizontal" role="form"><h4>'
+    	+ '<div class="form-group">'
+    		+ '<label class="col-lg-2 control-label">Propietario</label>'
+    		+ '<div class="col-lg-10"><p class="form-control-static">' + element.nombre + ' ' + element.apellido + '</p></div>'
+    	+ '</div>'
+
+    	+ '<div class="form-group">'
+    		+ '<label class="col-lg-2 control-label">Monto</label>'
+    		+ '<div class="col-lg-10"><p class="form-control-static">' + element.monto + '</p></div>'
+    	+ '</div>'
+    	+ '<div class="form-group">'
+    		+ '<label class="col-lg-2 control-label">Nro Referencia</label>'
+    		+ '<div class="col-lg-10"><p class="form-control-static">' + element.nro_referencia + '</p></div>'
+    	+ '</div>'
+    	+ '<div class="form-group">'
+    		+ '<label class="col-lg-2 control-label">Tipo de pago</label>'
+    		+ '<div class="col-lg-10"><p class="form-control-static">' + element.tipo + '</p></div>'
+    	+ '</div>'
+    	+ '<div class="form-group">'
+    		+ '<label class="col-lg-2 control-label">Banco</label>'
+    		+ '<div class="col-lg-10"><p class="form-control-static">' + element.banco + '</p></div>'
+    	+ '</div>'
+    	+ '<div class="form-group">'
+    		+ '<label class="col-lg-2 control-label">Fecha</label>'
+    		+ '<div class="col-lg-10"><p class="form-control-static">' + element.fecha + '</p></div>'
+    	+ '</div>'
+    	+ '<div class="form-group">'
+    		+ '<label class="col-lg-2 control-label">Estado</label>'
+    		+ '<div class="col-lg-10"><p class="form-control-static">' + element.estado + '</p></div>'
+    	+ '</div>'
+		+ '</h4></form></div>';
+    	$scope.showDialog({title: "<b>Detalles - Consignación #</b>" + element.id, message: message});
+    };
+
 	$scope.rechazarPago = function() {
+		$scope.loading = true;
 		//Editar en BD la consignacion id, y colocar estado en 2.
 	};
-
-    $scope.copiarPortaPapeles = function(elemento){
-    	console.log(elemento);
-    };
 
 });
