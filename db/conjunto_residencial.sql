49d6bdbef87b230de3a6a8d55c31e91bff4cc58f
Alfonso <osnoser_12@hotmail.com>
2014-06-08 12:18:38 -0430

    - Arreglado problema al conectarse a Yii, desde proyectos con diferentes nombre. - Organización de algunas consultas para crear un estandar en el diseño de la base de datos. - Agregado archivo "Orden de importación.txt", que indica el orden y dependencias de cada módulo.

---
+++ db/conjunto_residencial.sql
@@ -0,0 +1,23 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.4.1
+-- http://www.phpmyadmin.net
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 29-05-2014 a las 01:53:51
+-- Versión del servidor: 5.6.11
+-- Versión de PHP: 5.5.3
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de datos: `conjunto_residencial`
+--
+
+CREATE DATABASE IF NOT EXISTS `conjunto_residencial` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+
