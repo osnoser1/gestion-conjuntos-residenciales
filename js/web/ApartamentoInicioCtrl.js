4f4b6d9e8ce388a0ac9b28654aee006944f0efb8
TataLezama <TataLezama@TataLezama-PC>
2014-06-08 05:52:18 -0430

    agrege los controladores y lista de edificio, agregar y pagina de inicio del modulo de apartamento

--- js/web/ApartamentoInicioCtrl.js
+++ js/web/ApartamentoInicioCtrl.js
@@ -3,52 +3,30 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+'use strict';
 var myApp = angular.module('myApp');
 
-myApp.controllerProvider.register('ApartamentoInicioCtrl', function($scope, $http, $q, $filter, $timeout, $rootScope) {
-    /*$scope.today = function() {
-     $scope.dt = new Date();
-     };
-     $scope.today();
-     $scope.clear = function() {
-     $scope.dt = null;
-     };
-
-     $scope.ListarApartamento = function(element) {
-
-
-     var mensaje = '<form class="form-horizontal" role="form">'
-     + '<h1> Lista Apartamento</h1>'
-     + '<div class="list-group text-center">'
-     + '<div class="list-group-item row no-select active cabecera">'
-     + '<span class="badge pull-left">#</span>'
-     + '<p ng-dblclick="" class="col-xs-2">Id</p>'
-     + '<p ng-dblclick="" class="col-xs-2">Tipo</p>'
-     + '<p ng-dblclick="" class="col-xs-2">Tamano</p>'
-     + '<p ng-dblclick="" class="col-xs-2">NumHabitaciones</p>'
-     + '<p ng-dblclick="" class="col-xs-2">NumBanos</p>'
-     + '<p ng-dblclick="" class="col-xs-2">Sala</p>'
-     + '<p ng-dblclick="" class="col-xs-2">Comedor</p>'
-     + '<p ng-dblclick="" class="col-xs-2">Cocina</p>'
-     + '<p ng-dblclick="" class="col-xs-2">Lavandero</p>'
-     + '</div>'
-     + '<div class="list-group">'
-     + '<div class="list-group-item row no-select" ng-repeat="element in datos.Apartamentos" >'
-     + '<p class="col-xs-1">{{element.idApartamento}}</p>'
-     + '<p class="col-xs-2 pointer">' + element.TipoApartamento + '</p>'
-     + '<p class="col-xs-2 pointer">' + element.Tamano + '</p>'
-     + '<p class="col-xs-2 pointer">' + element.NumHabitaciones + '</p>'
-     + '<p class="col-xs-2 pointer">' + element.NumBanos + '</p>'
-     + '<p class="col-xs-2 pointer">' + element.Sala + '</p>'
-     + '<p class="col-xs-2 pointer">' + element.Comedor + '</p>'
-     + '<p class="col-xs-2 pointer">' + element.Cocina + '</p>'
-     + '<p class="col-xs-2 pointer">' + element.Lavandero + '</p>'
-     + '</div>'
-     + '</div>'
-     + '</div>'
-     + '</form>';
-     $scope.showDialog({title: "Listar Apartamento", messange: mensaje});
-     };*/
+myApp.controllerProvider.register('ApartamentoInicioCtrl', function($scope, $http, $q, $filter, $timeout, $rootScope, $location) {
+    $scope.today = function() {
+        $scope.dt = new Date();
+    };
+    $scope.today();
+    $scope.clear = function() {
+        $scope.dt = null;
+    };
+    $scope.datos = {
+        Apartamentos: [
+            {idApartamento: "A4H", TipoApartamento: "Familiar", Tamano: "250 mt2", NumHabitaciones: "3", NumBanos: "2", Sala: "Si", Comedor: "Si", Cocina: "Si", Lavandero: "Si"},
+            {idApartamento: "BpbW", TipoApartamento: "Estudio", Tamano: "150 mt2", NumHabitaciones: "1", NumBanos: "1", Sala: "Si", Comedor: "No", Cocina: "Si", Lavandero: "No"},
+            {idApartamento: "C7A", TipoApartamento: "Thomw House", Tamano: "450 mt2", NumHabitaciones: "5", NumBanos: "4", Sala: "Si", Comedor: "Si", Cocina: "Si", Lavandero: "Si"},
+            {idApartamento: "F2C", TipoApartamento: "Estudio", Tamano: "150 mt2", NumHabitaciones: "1", NumBanos: "1", Sala: "Si", Comedor: "No", Cocina: "Si", Lavandero: "No"},
+            {idApartamento: "IpbJ", TipoApartamento: "Familiar", Tamano: "250 mt2", NumHabitaciones: "3", NumBanos: "2", Sala: "Si", Comedor: "Si", Cocina: "Si", Lavandero: "Si"},
+        ],
+    };
+    $scope.ListarApartamento = function(element) {
+        $rootScope.activado = true;
+        $rootScope.datos = "0";
+        $location.path("panel/listado-apartamentos");
+    };
 });
 
