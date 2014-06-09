5842f8825eb2f48661ebc08a03bd457cfe3fbbbb
Felicce <exosymmetry@hotmail.com>
2014-06-09 17:36:49 -0430

    Modulo Secciones

--- js/web/ListadoSeccionesCtrl.js
+++ js/web/ListadoSeccionesCtrl.js
@@ -3,35 +3,71 @@
 var myApp = angular.module('myApp');
 
 myApp.controllerProvider.register('ListadoSeccionesCtrl', function($scope, $http, $q, $filter, $timeout, $rootScope) {
-    
-        
 
     $scope.datos = {
-        seccion: [
-                   {"id": "1", "Titulo": "Informacion", "Contenido":"hola mucho gusto"},
-                   {"id": "2", "Titulo": "Informacion", "Contenido":"hola mucho gusto"},
-		],
+        seccion: [],
     };
     
-    $scope.isCollapsed = true;
+    $scope.isCollapsed = false;
     
     $scope.entrarSeccion = function(elemento) {
-        $rootScope.seccion = {id:elemento.id,Titulo:elemento.Titulo, Contenido:elemento.Contenido};
+        $rootScope.seccion = {id:elemento.id,titulo:elemento.titulo, contenido:elemento.contenido};
         $scope.showConfirmDialog({title: "Modificar", src: "'partials/modificar-seccion.html'"}, $scope.ModificarSeccion);
     };
-    /*$scope.ModificarSeccion = function(){
-        console.log("Guardar");
+    $scope.verSeccion = function(elemento) {
+        $scope.showDialog({title: elemento.titulo, message: elemento.contenido});
+    };
+    
+    $scope.ModificarSeccion = function(elemento){
+        $.ajax
+        ({
+            type: "POST",
+            url: "models/consultas-crearseccion.php",
+            data: {id: 3, idseccion: $rootScope.seccion.id, titulo: $rootScope.seccion.titulo, contenido: $rootScope.seccion.contenido},
+            async: false,
+            dataType: "json",
+            success:
+            function(msg)
+            {
+                console.log("ok");
+                console.log(msg);
+            },
+            error:
+            function(msg) {
+                console.log("Error");
+                console.log(msg);
+            }
+        });
+
         var array = $filter('filter')($scope.datos.seccion, {id: $rootScope.seccion.id}, true);
         if(array.length !== 0){
-            array[0].Titulo = $rootScope.seccion.Titulo;
-            array[0].Contenido = $rootScope.seccion.Contenido;
+            array[0].titulo = $rootScope.seccion.titulo;
+            array[0].contenido = $rootScope.seccion.contenido;
         }
         $('#myModal').modal('hide');        
     };
-    $scope.ListadoSeccion = function(elemento){
-        
-        
-        
-    };*/
-    
+    $scope.ListadoSeccion = function(){
+        $.ajax
+        ({
+            type: "POST",
+            url: "models/consultas-crearseccion.php",
+            data: {id: 2},
+            async: false,
+            dataType: "json",
+            success:
+            function(msg)
+            {
+                for (var i = 0; i < msg.length; i++)
+                {
+                    $scope.datos.seccion.push(msg[i]);
+                }
+            },
+            error:
+            function(msg) {
+                console.log(msg);
+            }
+        });
+    };
+    $scope.ListadoSeccion();
+
 });
