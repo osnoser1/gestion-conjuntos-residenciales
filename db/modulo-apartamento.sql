3dcf1c07a25f9eed842c0187c8348270f8ecb6f8
Alfonso <osnoser_12@hotmail.com>
2014-06-24 09:53:34 -0430

    - Cambio nombre tabla de 'apartamento-usuario' a 'apartamento_usuario'. - Cambiada codificación a utf8 en apartamento_usuario.

--- db/modulo-apartamento.sql
+++ db/modulo-apartamento.sql
@@ -95,17 +95,17 @@ INSERT INTO `apartamentotipo` (`idApartamentoTipo`, `Nombre`, `Tamano`, `numBano
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `apartamento-usuario`
+-- Estructura de tabla para la tabla `apartamento_usuario`
 --
 
-CREATE TABLE IF NOT EXISTS `apartamento-usuario` (
+CREATE TABLE IF NOT EXISTS `apartamento_usuario` (
   `idusuarioapartamento` int(11) NOT NULL AUTO_INCREMENT,
   `idusuario` int(11) NOT NULL,
   `idapartamento` int(11) NOT NULL,
   PRIMARY KEY (`idusuarioapartamento`),
   UNIQUE KEY `idapartamento` (`idapartamento`),
   UNIQUE KEY `idusuario` (`idusuario`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
 --
 -- Restricciones para tablas volcadas
@@ -118,9 +118,9 @@ ALTER TABLE `apartamento`
   ADD CONSTRAINT `apartamento_ibfk_2` FOREIGN KEY (`idTipo`) REFERENCES `apartamentotipo` (`idApartamentoTipo`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `apartamento_ibfk_1` FOREIGN KEY (`idEdificio`) REFERENCES `edificio` (`idEdificio`) ON DELETE CASCADE ON UPDATE CASCADE;
 
-ALTER TABLE `apartamento-usuario`
+ALTER TABLE `apartamento_usuario`
   ADD CONSTRAINT `fk_apartamento_usuario_apartamento` FOREIGN KEY (`idapartamento`) REFERENCES `apartamento` (`idApartamento`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `apartamento-usuario_ibfk_1` FOREIGN KEY (`idusuario`) REFERENCES `usuario` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `apartamento_usuario_ibfk_1` FOREIGN KEY (`idusuario`) REFERENCES `usuario` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
