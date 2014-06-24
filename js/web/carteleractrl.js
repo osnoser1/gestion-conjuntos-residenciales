f29b252c2c290a739c71f4ab5d16405bef851c6c
helyson <lysonxd@gmail.com>
2014-06-24 14:55:36 -0430

    cartelera control

--- js/web/carteleractrl.js
+++ js/web/carteleractrl.js
@@ -225,10 +225,10 @@ function eventos (){
                   function (msg) 
                   {      
                     for (var i =msg[0].m-1; i>=0; i--) {
-                       $('#contenedor2').append('<div><h4><b>'+msg[i].titulo+'</b></h4></div>');
+                       $('#contenedor2').append('<div><h4><b>'+msg[i].titulo+'</b></h4><button class="eliminar btn btn-primary btn-xs" data-toggle="modal" data-target="#myModaleliminar" style="position: absolute; right:25px;">X</button></div>');
                        $('#contenedor2').append('<div style="height:120px;  overflow: auto; border:1px solid #ccc; border-top-left-radius: 4px;     border-bottom-left-radius: 4px;     border-top-right-radius: 4px border-bottom-right-radius: 4px;" class="form-control">'+msg[i].contenido+'</div>');
                        $('#contenedor2').append('<a class="btn btn-default btn-xs" href="#/panel/crear-mensaje"><span class="glyphicon glyphicon-envelope"></span></a>');   
-                       $('#contenedor2').append('<button class="btn btn-default btn-xs" data-toggle="modal" data-target="#myModaleliminar">Eliminar</button>');
+                       //$('#contenedor2').append('<button class="btn btn-default btn-xs" data-toggle="modal" data-target="#myModaleliminar">Eliminar</button>');
                        $('#contenedor2').append('<label style="position: absolute; right:25px;">'+msg[i].fecha+'</label><br>');
                     }
                             
@@ -237,4 +237,38 @@ function eventos (){
                   function (msg) {alert( msg +"No se pudo realizar la conexion");}
                   });
       }
-}
+       $('#confirmacion').click(function(){             
+          $.ajax
+            ({
+            type: "POST",
+            url: "models/cartelera.php",
+            data: {id:6, idpost:idpostEliminar},
+            async: false,
+            dataType: "json",
+            success:
+            function (msg) 
+            {       
+          $('#myModaleliminar').modal('hide');
+          if(msg=="true"){
+            $('#mensajeabierto').fadeOut(function(){
+              $('#mensajes').fadeIn();
+              show({message: {text: "El Mensaje ha sido eliminado exitosamente"}, type: 'success'});
+              $('#' + id).remove();
+            });
+            
+            
+          }   
+            },
+            error:
+            function (msg) {alert( msg +"No se pudo realizar la conexion");}
+            });
+
+        });
+      $('.eliminar').click(function(){
+            id=$(this).attr('name');
+            idpostEliminar=id;
+            console.log("click en boton" + id);
+            $('#myModaleliminar').modal('show');
+
+          });
+      var idpostEliminar="";
