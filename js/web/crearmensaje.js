dce7edeac8cd06bff9113667250a7ad7a3b008c3
cheche338 <cheche338@gmail.com>
2014-06-29 07:31:40 -0430

    tabla sql usuario-apartamento

--- js/web/crearmensaje.js
+++ js/web/crearmensaje.js
@@ -94,7 +94,7 @@ function eventos () {
             },
             onChange: function(element, checked) {
                idEdificio=Edificios2.value;
-              console.log(idEdificio);
+             // console.log(idEdificio);
               $('#Pisos').empty();
               cargarpisos();
             }
@@ -113,17 +113,17 @@ function eventos () {
                   function (msg) 
                   {    
                       
-                              Pisos.options[0]= new Option ("");
-                              Pisos.options[0].text = "";
-                              Pisos.options[0].value ="-1" ; 
-                              for(i=0; i < msg[0].m; i++)
-                              {
-                                Pisos.options[i+1]= new Option ("Piso " + msg[i].idPiso);
-                                Pisos.options[i+1].text ="Piso " + msg[i].idPiso ;
-                                Pisos.options[i+1].value = msg[i].idPiso; 
-                              }                              
-                           
-                          $('#Pisos').multiselect('rebuild');
+                      Pisos.options[0]= new Option ("");
+                      Pisos.options[0].text = "";
+                      Pisos.options[0].value ="-1" ; 
+                      for(i=0; i < msg[0].m; i++)
+                      {
+                        Pisos.options[i+1]= new Option ("Piso " + msg[i].idPiso);
+                        Pisos.options[i+1].text ="Piso " + msg[i].idPiso ;
+                        Pisos.options[i+1].value = msg[i].idPiso; 
+                      }                              
+                   
+                    $('#Pisos').multiselect('rebuild');
                   },
                   error:
                   function (msg) {alert( msg +"No se pudo realizar la conexion");}
@@ -168,9 +168,7 @@ function eventos () {
                   error:
                   function (msg) {alert( msg +"No se pudo realizar la conexion");}
                   });
-                
-
-              }
+                }
            });
 
            
@@ -194,11 +192,13 @@ function eventos () {
                   success:
                   function (msg) 
                   {    
-                    console.log("Piso: "+ Pisos2.value + "Numero de apartamentos" + msg[0].m);
+                    //console.log("Piso: "+ Pisos2.value + "Numero de apartamentos" + msg[0].m);
                     for(i=0; i < msg[0].m; i++)
                     {
                       Apartamentos.options[i]= new Option ("Apartamento" + msg[i].idapartamento);
                       Apartamentos.options[i].text ="Apartamento "+ msg[i].Nombre;
+                      Apartamentos.options[i].value = msg[i].idapartamento; 
+
                     } 
                     $('#Apartamentos').multiselect('rebuild');
                     
@@ -210,119 +210,31 @@ function eventos () {
             });
 
         
-       // $.ajax
-       //  ({
-       //  type: "POST",
-       //  url: "models/consultas-crearMensaje.php",
-       //  data: {id:1},
-       //  async: false,
-       //  dataType: "json",
-       //  success:
-       //  function (msg) 
-       //  {       
-         
-       //      Edificios.options[0]= new Option ("");
-       //      Edificios.options[0].text = "";
-       //      Edificios.options[0].value ="-1" ; 
-       //    for(i=0; i < msg[0].m; i++)
-       //    {
-
-       //      Edificios.options[i+1]= new Option (msg[i].nombre);
-       //      Edificios.options[i+1].text = msg[i].nombre;
-       //      Edificios.options[i+1].value = msg[i].idEdificio; 
-
-       //    }   
-
-
-     
-
-
-       //  },
-       //  error:
-       //  function (msg) {alert( msg +"No se pudo realizar la conexion");}
-       //  });
+        $(document).on('click', '#apartamentos', (function(e) {
+          $('#Apartamentos').multiselect({
+              enableFiltering: true
+            });
 
         
-
-                     
-
-       //  Edificios.onchange = function () 
-       //    { 
-       //      idEdificio=Edificios.value;
-       //      $('#Pisos').empty();
-       //      $('#Apartamentos').empty();
-       //      $.ajax
-       //            ({
-       //            type: "POST",
-       //            url: "models/consultas-crearMensaje.php",
-       //            data: {id:2, idEdificio:Edificios.value},
-       //            async: false,
-       //            dataType: "json",
-       //            success:
-       //            function (msg) 
-       //            {    
-                    
-       //              for(i=0; i < msg[0].m; i++)
-       //              {
-       //                Pisos.options[i]= new Option ("Piso" + msg[i].idPiso);
-       //                Pisos.options[i].text ="Piso" + msg[i].idPiso;
-       //                Pisos.options[i].value = msg[i].idPiso; 
-       //              } 
-                      
-                      
-       //            },
-       //            error:
-       //            function (msg) {alert( msg +"No se pudo realizar la conexion");}
-       //            });
-       //    }
-        //   Pisos.onchange = function () 
-        //   {   idPiso=Pisos.value;
-        //        $('#Apartamentos').empty();
-        //         $.ajax
-        //           ({
-        //           type: "POST",
-        //           url: "models/consultas-crearMensaje.php",
-        //           data: {id:3, idEdificio:Edificios.value, idPiso:Pisos.value},
-        //           async: false,
-        //           dataType: "json",
-        //           success:
-        //           function (msg) 
-        //           {    
-        //             console.log("Piso: "+ Pisos.value + "Numero de apartamentos" + msg[0].m);
-        //             for(i=0; i < msg[0].m; i++)
-        //             {
-        //               Apartamentos.options[i]= new Option ("Apartamento" + msg[i].idapartamento);
-        //               Apartamentos.options[i].text ="Apartamento " + msg[i].Nombre ;
-        //               Apartamentos.options[i].value = msg[i].idUsuario; 
-        //             } 
-                      
-        //           },
-        //           error:
-        //           function (msg) {alert( msg +"No se pudo realizar la conexion");}
-        //           });
-        //   }
-
-        //   Apartamentos.onclick=function(){
-        //        $.ajax
-        //           ({
-        //           type: "POST",
-        //           url: "models/consultas-crearMensaje.php",
-        //           data: {id:4, idUsuario:Apartamentos.value},
-        //           async: false,
-        //           dataType: "json",
-        //           success:
-        //           function (msg) 
-        //           {    
-
-        //                console.log("para " + para.value + " email: "+msg[0].email);
-        //                para.value+=msg[0].email+",";
-        //                $("#Apartamentos option[value="+Apartamentos.value+"]").remove();
-                      
-        //           },
-        //           error:
-        //           function (msg) {alert( msg +"No se pudo realizar la conexion");}
-        //           });
-
-        //   }
-
+          if ($('#porpisos').attr('class')=="active") {
+              if($('#Pisos').val()!=null){
+                  console.log("por pisos " + $('#porpisos').attr('class'));
+                  console.log($('#Pisos').val());
+              }
+              
+          }
+          if($('#porapartametos').attr('class')=="active"){
+            if($('#Apartamentos').val()!=null){
+              console.log("por apartamentos " +$('#porapartametos').attr('class'));
+              console.log($('#Apartamentos').val());
+            }
+                  
+          }
+          if($('#poredificios').attr('class')=="active"){
+             if($('#Edificios').val()!=null){
+              console.log("por edificios " + $('#poredificios').attr('class'));
+               console.log($('#Edificios').val());
+            }
+          }
+        }));
 }
