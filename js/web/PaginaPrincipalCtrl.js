bc35d9105497cdbc90d5c9aaa8d38651cb09a856
Gabriel <Gabriel@Gabriel-HP>
2014-06-30 21:25:43 -0430

    Vista Principal Agrregando orden de importancion y más cambios parciales Modificar pagina principal

--- js/web/PaginaPrincipalCtrl.js
+++ js/web/PaginaPrincipalCtrl.js
@@ -8,44 +8,43 @@ myApp.controllerProvider.register('PaginaPrincipalCtrl', function($scope, $http,
     $scope.datos = {
         secciones: [],
         seccion: {
-            titulo: "hola",
         },
     };
 
     $.ajax
-    ({
-        type: "POST",
-        url: "models/consultas-crearseccion.php",
-        data: {id:6},
-        async: true,
-        dataType: "json",
-        success:
-        function(msg)
-        {
-       
-            footer.innerHTML=msg[0].footer;
-            Titulo.innerHTML=msg[0].titulo;
-          
-      },
-      error:
-      function (msg) {alert( msg +"No se pudo realizar la conexion");}
-      });
-/*    $.ajax
-    ({
-        type: "POST",
-        url: "models/consultas-crearseccion.php",
-        data: {id:8},
-        async: true,       
-        success:
-        function(msg)
-        {       
-            footer2.innerHTML='<img src='+msg+' class="img-responsive img-thumbnail" />'
-            $("body").css("background-image","url(partials/14256282-rostro-humano-pintado-con-la-bandera-de-colombia.jpg)");
-      },
-      error:
-      function (msg) {alert( msg +"No se pudo realizar la conexion");}
-      });*/
- 
+            ({
+                type: "POST",
+                url: "models/consultas-crearseccion.php",
+                data: {id: 6},
+                async: true,
+                dataType: "json",
+                success:
+                        function(msg)
+                        {
+                            footer.innerHTML = msg[0].footer;
+                            Titulo.innerHTML = msg[0].titulo;
+                        },
+                error:
+                        function(msg) {
+                            alert(msg + "No se pudo realizar la conexion");
+                        }
+            });
+    /*    $.ajax
+     ({
+     type: "POST",
+     url: "models/consultas-crearseccion.php",
+     data: {id:8},
+     async: true,
+     success:
+     function(msg)
+     {
+     footer2.innerHTML='<img src='+msg+' class="img-responsive img-thumbnail" />'
+     $("body").css("background-image","url(partials/14256282-rostro-humano-pintado-con-la-bandera-de-colombia.jpg)");
+     },
+     error:
+     function (msg) {alert( msg +"No se pudo realizar la conexion");}
+     });*/
+
 
     $scope.ListadoSeccion = function() {
         $.ajax
