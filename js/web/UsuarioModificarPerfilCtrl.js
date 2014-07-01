a9e31004e23fe67c1fbeb68691bffdf7450d2759
MariaElena <MariaElena@190.37.191.176>
2014-06-30 20:32:41 -0430

    terminado vista modificar contrasena

--- js/web/UsuarioModificarPerfilCtrl.js
+++ js/web/UsuarioModificarPerfilCtrl.js
@@ -8,11 +8,38 @@
 var myApp = angular.module('myApp');
 
 myApp.controllerProvider.register('UsuarioModificarPerfilCtrl', function($scope, $http, $q, $timeout, $rootScope, $location) {
+    console.log("UsuarioModificarPerfilCtrl");
     $scope.datos = {
     };
+    $scope.usuario = {};
 
-    $scope.ModificarPerfil = function(datosmodificados) {
-        $http.post(url + 'usuario/modificarPerfil', $.param({datos: datosmodificados}), {timeout: 5000, responseType: "json", headers: {'Content-Type': 'application/x-www-form-urlencoded'}}
+    $scope.mostrarDatos = function(data) {
+        $http.post(url + 'usuario/detalle', $.param({datos: data}), {timeout: 5000, responseType: "json", headers: {'Content-Type': 'application/x-www-form-urlencoded'}}
+        ).success(function(data, status, headers, config) {
+            console.dir(data);
+            if (typeof data !== 'object' || !data.respuesta) {
+                $scope.error(data, status, headers, config);
+            }
+            $scope.usuario = data.datos;
+            //console.log($scope.usuario);
+        }).error($scope.error);
+    };
+
+    $http.get(url + 'usuario/usuarioLogueado').success(function(data) {
+        //console.log(data);
+        if (typeof data !== 'undefined') {
+            //console.log("logeado");
+            $scope.mostrarDatos(data);
+        }
+    });
+    $scope.addTelefono = function(datos) {
+        $scope.usuario.telefonos.push(datos.nuevoTelefono);
+        datos.nuevoTelefono = {};
+    };
+    $scope.ModificarPerfil = function(usuario) {
+        console.log(usuario);
+        //return;
+        $http.post(url + 'usuario/modificarPerfil', $.param({datos: usuario}), {timeout: 5000, responseType: "json", headers: {'Content-Type': 'application/x-www-form-urlencoded'}}
         ).success(function(data, status, headers, config) {
             console.dir(data);
             if (typeof data === "object" && data.respuesta) {
