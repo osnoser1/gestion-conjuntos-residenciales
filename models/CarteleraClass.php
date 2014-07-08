c92f33edb838dac5c4edf0fc30451949adad1650
hely <hely@201.242.174.63>
2014-07-08 08:56:13 -0430

    pasando a clases

--- models/CarteleraClass.php
+++ models/CarteleraClass.php
@@ -19,6 +19,7 @@ class CarteleraClass {
     public function IngresarporApartamento($apartamentos,$titulo,$contenido){
 	session_start();
         date_default_timezone_set('America/Caracas');
+        $this->mysqli = new mysqli(Host, User, Pass, BasedeDatos);
         $this->apartamentos=serialize($apartamentos);
 	$this->nombre=$_SESSION["Nombre"];
         $this->apellido=$_SESSION["Apellido"];
